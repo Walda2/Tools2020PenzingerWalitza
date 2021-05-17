3c4d4460ab1803c51ca0b10baeba343f4d8251c8
Walda2 <david.walitza@outlook.com>
2021-05-17 18:34:31 +0200

    added lines of code and a method

--- spenzinger_walda2_android/src/spenzinger_walda2_android/test.java
+++ spenzinger_walda2_android/src/spenzinger_walda2_android/test.java
@@ -5,10 +5,17 @@ public class test {
 	public void thisIsANewMethod() {
 		System.out.println("Dies ist die Zeile von spenzinger");
 		System.out.println("new line of code");
+		System.out.println("another line");
+		System.out.println("hi ..");
 	}
 	
 	private int testMethod() {
 		System.out.println("Hallo");
 		System.out.println("New Line");
 	}
+	
+	public void methodForNothing() {
+		
+	}
+	
 }
